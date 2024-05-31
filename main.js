6b3e8e7035a059480f3a7c6b36dc0ab3fed75cb6
urixqt22 <urixfarinas@gmail.com>
2024-05-31 09:44:17 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+//Show Menu
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId);
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
+        });
+    }
+}
+showMenu('nav_toggle', 'nav_menu');
+
+const navLink = document.querySelectorAll('.nav_link'); 
+function linkAction() {
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    const navMenu = document.getElementById('nav_menu');
+    navMenu.classList.remove('show');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
